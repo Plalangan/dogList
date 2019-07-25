4fc0edda7d7b7aa5806dbe09361948659dddec3f
Plalangan <P.lalangan@hotmail.com>
2019-07-25 00:04:56 -0700

    Load List
    fixed loadlist function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 
 
-var $dogList = $('ul');
+var $dogList = $('<ul></ul>').addClass('dogList');
 
-var dogRepository = (function (){
+
+  var dogRepository = (function (){
   var repository = [];
   var apiUrl = 'https://dog.ceo/api/breeds/list/all';
-  var $dogList = $('ul');
+
 
   function add(dog) {
     repository.push(dog);
@@ -17,59 +18,63 @@ var dogRepository = (function (){
 
   function addListItem(dog){
     var $addListItem = $('<li></li>');
-    var $button = $('<button class = "button">' +  dog.name  + '</button>');
+    var $button = $('<button class = "button"></button>').text(dog.name);
     $addListItem.append($button);
-    $button.on('click', showModal);
+    $button.click(showModal);
     $dogList.append($addListItem);
 
 
   }
 
-  function showDetails(dog) {
+/*function showDetails(dog){
     dogRepository.loadDetails(dog).then(function () {
     showModal(dog);   })};
+*/
 
-  function loadList(){
-    return $.ajax(apiUrl, {dataType: 'json'}).then(function(item){
+function loadList(){
+      return $.ajax(apiUrl, {dataType: 'json'}).then(function(item){
 
-      $.each(item.results, function(index, item){
-        //Uncomment the line below to see index in the callback function in $.each()
-        //console.log('response object ', index);
-        var dog = {
-          name: item.name,
-          detailsUrl: item.url
-        }
-        add(dog)
-      })
 
+
+        $.each(item.message, function(dog){
+          addListItem(dog, item.message[dog]);
+          //Uncomment the line below to see index in the callback function in $.each()
+
+          var dog = {
+            name: item.message,
+          }
+          add(dog)
+        });
+
+        }).catch(function(e){
+        console.error(e);
+      });
+    }
+
+
+  /*  function loadDetails(item){
+      var url = item.detailsUrl;
+      return $.ajax(url).then(function(details){
+        //Uncomment the line below to log index
+        console.log('Item details', details);
+        // Now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = Object.keys(details.types);
       }).catch(function(e){
-      console.error(e);
-    });
-  }
-
-
-  function loadDetails(item){
-    var url = item.detailsUrl;
-    return $.ajax(url).then(function(details){
-      //Uncomment the line below to log index
-      //console.log('Item details', details);
-      // Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = Object.keys(details.types);
-    }).catch(function(e){
-      console.error(e);
-    });
-  }
+        console.error(e);
+      });
+    }
+*/
 
     function showModal(dog){
     var $modalContainer = $('<div class = "modal-container"></div>');
 
     var $modal = $('<div class = "modal"><div>');
-    var $closeButtonElement = $('<button class = "closeButton">Close<button>');
-  //  $closeButtonElement.on('click', hideModal);
+    var $closeButtonElement = $('<button class = "closeButton">Close</button>').click(hideModal);
 
-    var $nameElement = $('<h1>' + item.name + ' </h1>');
+
+    var $nameElement = $('<h1></h1>').text('dog.name');
     $modal.append($closeButtonElement);
     $modal.append($nameElement);
     $modalContainer.append($modal);
@@ -79,18 +84,18 @@ var dogRepository = (function (){
   }
 
   function hideModal(){
-    var $modalContainer = $('#modal-container');
+    var $modalContainer = $('.modal-container');
     $modalContainer.removeClass('is-visible');
   }
 
   $('window').on('keydown', function(e){
-    var $modalContainer = $('#modal-container');
+    var $modalContainer = $('.modal-container');
     if (e.key === 'Escape' && $modalContainer.hasClass('is-visible')){
       hideModal();
     }
   });
 
-  var $modalContainer = $('#modal-container');
+  var $modalContainer = $('.modal-container');
   $modalContainer.on('click', (e) => {
     var target = e.target;
     if (target === $modalContainer){
@@ -106,26 +111,23 @@ var dogRepository = (function (){
     getAll: getAll,
     addListItem: addListItem,
     loadList: loadList,
-    loadDetails: loadDetails,
+  //  loadDetails: loadDetails,
     showModal: showModal,
     hideModal: hideModal
 
 
 
 
-  }
-})();
+  };
+}
+)
+();
 
 
-dogRepository.loadList().then(function() {
-  // Now the data is loaded!
-dogRepository.getAll().forEach(function(){
-    dogRepository.addListItem(dog);
-  });
-});
-/*
+dogRepository.loadList()
 
-dogRepository.showDetails(dog);
 
-dogRepository.showModal(dog);
-*/
+
+
+
+dogRepository.showModal();
